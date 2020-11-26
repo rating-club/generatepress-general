f614490da05329099894872d83cc88468abda929
Milan Petrovic <milan@gdragon.info>
2020-11-26 22:45:04 +0100

    Few more features.

--- functions.php
+++ functions.php
@@ -1,19 +1,49 @@
 <?php
 
+/** GD Rating System Pro
+ * Display slider rating value badge in the archives.
+ */
+add_action( 'generate_after_entry_content', 'gdrts__generate_after_entry_content_slider_badge', 5 );
+function gdrts__generate_after_entry_content_slider_badge() {
+	if ( is_post_type_archive( 'article' ) ) {
+		$item   = gdrts_get_rating_item_by_post();
+		$rating = $item->get_method_data( 'slider-rating' );
+
+		if ( ! empty( $rating ) ) {
+			echo '<div class="slider-rating-archive">';
+			echo '<span>' . absint( $rating['rating'] ) . '%</span>';
+			echo sprintf( _n( "%s vote", "%s votes", $rating['votes'] ), $rating['votes'] );
+			echo '</div>';
+		}
+	}
+}
+
+/** GD Rating System Pro
+ * Display active stars rating block in the archives.
+ */
+add_action( 'generate_after_entry_content', 'gdrts__generate_after_entry_content_stars_block', 5 );
+function gdrts__generate_after_entry_content_stars_block() {
+	if ( is_posts_page() ) {
+		echo '<div class="stars-rating-archive">';
+		gdrts_posts_render_rating( array( 'echo' => true ) );
+		echo '</div>';
+	}
+}
+
 /** GeneratePress
-  * Load template for aggregated ratings to display after news post type archive.
-  */
-add_action('generate_after_archive_title', 'gdrts__generate_after_archive_title');
+ * Load template for aggregated ratings to display after news post type archive.
+ */
+add_action( 'generate_after_archive_title', 'gdrts__generate_after_archive_title' );
 function gdrts__generate_after_archive_title() {
-	if (is_post_type_archive('news')) {
-		require_once('parts/news-aggregated.php');
+	if ( is_post_type_archive( 'news' ) ) {
+		require_once( 'parts/news-aggregated.php' );
 	}
 }
 
 /** GeneratePress:
-  * Enable entry meta block for 'news' and 'article' post types.
-  */
-add_filter('generate_entry_meta_post_types', 'gdrts__generate_entry_meta_post_types' );
+ * Enable entry meta block for 'news' and 'article' post types.
+ */
+add_filter( 'generate_entry_meta_post_types', 'gdrts__generate_entry_meta_post_types' );
 add_filter( 'generate_footer_meta_post_types', 'gdrts__generate_entry_meta_post_types' );
 function gdrts__generate_entry_meta_post_types( $types ) {
 	$types[] = 'news';
@@ -23,8 +53,8 @@ function gdrts__generate_entry_meta_post_types( $types ) {
 }
 
 /** WordPress:
-  * Add 'news' and 'article' post types into category and tags archives.
-  */
+ * Add 'news' and 'article' post types into category and tags archives.
+ */
 add_filter( 'pre_get_posts', 'gdrts__pre_get_posts' );
 function gdrts__pre_get_posts( $query ) {
 	if ( is_category() || is_tag() ) {
