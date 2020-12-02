8756a4541241e00e8142f96480ef863619a3ae89
Milan Petrovic <milan@gdragon.info>
2020-12-02 22:00:31 +0100

    Minor update.

--- parts/author-aggregated.php
+++ parts/author-aggregated.php
@@ -22,6 +22,7 @@
 
 	?>
     <p>
-        Rating: <strong><?php echo $rating->rating; ?></strong>/5, From <?php echo $rating->items; ?> posts and <?php echo $rating->votes; ?> votes.
+        Rating:
+        <strong><?php echo $rating->rating; ?></strong>/5, From <?php echo $rating->items; ?> posts and <?php echo $rating->votes; ?> votes.
     </p>
 </div>
